c1a0438fff331a1aca6ed360ac31e66641c3622f
ysw <ysw99@kakao.com>
2021-09-02 02:06:17 +0900

    페이지 Router 구현

--- client/src/App.js
+++ client/src/App.js
@@ -1,24 +1,29 @@
-import logo from './logo.svg';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link
+} from 'react-router-dom';
+import LandingPage from './components/views/LandingPage/LandingPage.js';
+import LoginPage from './components/views/LoginPage/LoginPage.js';
+import RegisterPage from './components/views/RegisterPage/RegisterPage.js';
+import NavBar from './components/views/NavBar/NavBar.js';
+import Footer from './components/views/Footer/Footer.js';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <div>
+        <NavBar />
+        <Switch>
+          <Route exact path = "/" component = {LandingPage} />
+          <Route exact path = "/login" component = {LoginPage} />
+          <Route exact path = "/register" component = {RegisterPage} />
+        </Switch>
+        <Footer />
+      </div>
+    </Router>
   );
 }
 
