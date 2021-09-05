77712ae0c125d58955351264d9f8694199ce5c3c
1417MHz <ysw99@kakao.com>
2021-09-05 21:41:49 +0900

    로그아웃 구현

--- client/src/App.js
+++ client/src/App.js
@@ -6,21 +6,19 @@ import {
 import LandingPage from './components/views/LandingPage/LandingPage.js';
 import LoginPage from './components/views/LoginPage/LoginPage.js';
 import RegisterPage from './components/views/RegisterPage/RegisterPage.js';
-import NavBar from './components/views/NavBar/NavBar.js';
-import Footer from './components/views/Footer/Footer.js';
+// import NavBar from './components/views/NavBar/NavBar.js';
+// import Footer from './components/views/Footer/Footer.js';
 import './App.css';
 
 function App() {
   return (
     <Router>
       <div>
-        {/* <NavBar /> */}
         <Switch>
           <Route exact path = "/" component = {LandingPage} />
           <Route exact path = "/login" component = {LoginPage} />
           <Route exact path = "/register" component = {RegisterPage} />
         </Switch>
-        {/* <Footer /> */}
       </div>
     </Router>
   );
